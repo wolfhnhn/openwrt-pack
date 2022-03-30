20de24e50b954e5f499c14576a1117381745b780
wolfhnhn <42992384+wolfhnhn@users.noreply.github.com>
2022-03-30 22:26:32 +0800

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+function git_sparse_clone() {
+branch="$1" rurl="$2" localdir="$3" && shift 3
+git clone -b $branch --depth 1 --filter=blob:none --sparse $rurl $localdir
+cd $localdir
+git sparse-checkout init --cone
+git sparse-checkout set $@
+mv -n $@ ../
+cd ..
+rm -rf $localdir
+}
+
+function mvdir() {
+mv -n `find $1/* -maxdepth 0 -type d` ./
+rm -rf $1
+}
+
+git clone --depth 1 git clone https://github.com/vernesong/OpenClash.git && mvdir OpenClash
+
+
+exit 0
